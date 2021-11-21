50980a0d25b65969d07013a4cf848cabdfb743d8
YPR <ypr@desktop-4hg6o4o>
2021-11-21 23:02:22 +0900

    github action 셋팅해보기

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,6 +8,7 @@ public class TestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TestApplication.class, args);
+    System.out.println("hello world!");
   }
 
 }
