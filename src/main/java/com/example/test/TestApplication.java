06aecf1f7173df84d73329c3b707625d7d0f0428
YPR <YPR@DESKTOP-4HG6O4O>
2021-11-21 23:25:14 +0900

    github slack 연동 확인 test

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -8,7 +8,7 @@ public class TestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TestApplication.class, args);
-    System.out.println("hello world!");
+    System.out.println("hello world!!!!!!!");
   }
 
 }
