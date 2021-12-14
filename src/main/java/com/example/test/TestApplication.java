ccea81594aa536b75e5a95ab4153acd13a2dbca8
promise_yoo <56250078+ypr821@users.noreply.github.com>
2021-12-14 15:40:31 +0900

    jacoco badge 설정 추가

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,8 +2,10 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "web")
 public class TestApplication {
 
   public static void main(String[] args) {
